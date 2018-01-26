16f3e235914293293038394b319f53246ffd2342
Areeves93 <32654508+Areeves93@users.noreply.github.com>
2018-01-25 19:39:59 -0500

    Initial commit

---
+++ testanswers.js
@@ -0,0 +1,78 @@
+//Final Exam
+
+
+## Questions
+
+1.
+//Pseudocode : Merge Routine
+//merge(L,R)
+//Rpointer = 0
+//Lpointer = 0
+//L[0] is greater than R[0]
+//  push lower number (R[0] to output array
+//  increment Rpointer
+//else push L[Lpointer] into output array
+//  increment Lpointer
+
+2. 
+function Ran(){
+    Math.floor((Math.random() * 50)) + 50
+  }
+
+3.
+   function whatever(){
+     Math.floor((Math.random()* 50)) - 50
+   }
+
+4.
+  function coinFlip(){
+      return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  }
+
+5.!10 || !x
+
+6. !a < !10 || !b > !5
+
+7.(a && x <= 10) || (a && y < 0)
+
+
+
+8. !a < !10 && !b < !10 && !c < !10 && isAdmin
+
+9.
+  function twodconcat(arr){
+    let arry = ['I think I got this',['Yup I got it'] ]
+  return concat(arry[0],array[1])
+  }
+```
+
+10.
+
+11. 
+
+12.
+
+13. 
+
+14. 
+
+15. Linear 
+
+
+16.Linear
+
+17. Linear
+
+
+18. a storage, a capacity and a counter.
+
+19. a storage, a head and tail, and also a capacity.
+
+20.A bubblesort is:
+Two numbers switch if the number is bigger than the one it is being compared to. Once the largest number is on top, the sort begins all over again until the set is all sorted.
+
+21. Create a personal website and upload it so that it is accessible at yourdomain.fvi-grad.com.  
+    1. The website must have your name, contact information, skills, and at least two links to portfolio items you are hosting on fvi-grad.
+    2. The website must not be in a subdirectory of the document root. ie. It needs to be accessible by going to yourusername.fvi-grad.com/ without anything after the slash.
+    3. You can and should use a template
+    4. Include the link to your website in your answers document -->
